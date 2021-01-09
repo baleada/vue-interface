a3fedfa7347365320bac2b898390e8b52c4b8296
AlexVipond <alex.r.vipond@gmail.com>
2021-01-09 10:26:20 -0600

    fixes package exports

--- rollup.config.js
+++ rollup.config.js
@@ -8,22 +8,16 @@ const shared = configureable('rollup')
         ])
         .resolve()
         .vue()
-        .virtualIndex('src/index.js', { test: ({ id }) => /src\/(?:components\/|symbols\/|)[^\/]+.(?:js|vue)$/.test(id) })
-        .virtualIndex('src/components')
-        .virtualIndex('src/symbols')
-        .virtualIndex('src/util'),
+        .virtual.index('src/index.js', { test: ({ id }) => /src\/(?:components\/|symbols\/|)[^\/]+.(?:js|vue)$/.test(id) })
+        .virtual.index('src/components')
+        .virtual.index('src/symbols')
+        .virtual.index('src/util'),
       esm = shared
         .delete({ targets: 'lib/*', verbose: true })
         .input('src/index.js')
         .esm({ file: 'lib/index.js', target: 'browser' })
-        .configure(),
-      pluginEsm = shared
-        .delete({ targets: 'plugin/*', verbose: true })
-        .input('src/plugin.js')
-        .esm({ file: 'plugin/index.js', target: 'browser' })
         .configure()
 
 export default [
   esm,
-  pluginEsm,
 ]
